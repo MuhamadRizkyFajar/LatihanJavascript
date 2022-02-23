d744e0a86cd1b83d749ede08b0d0d7107c47d9db
Muhamad Rizky Fajar <avangedsoul@gmail.com>
2022-02-23 22:53:15 +0700

    Latihan Javascript

---
+++ latihan.js
@@ -0,0 +1,13 @@
+alert('Hai!');
+alert('Nama');
+alert('Saya');
+alert('Chrome');
+var nama = prompt('siapa namamu?');
+alert('Hai '+nama+'!')
+var teman = confirm('apakah mau berteman denganku?');
+
+if (teman === true) {
+    alert('Selamat bertaman '+nama);
+} else {
+    alert('yahh sayang sekali');
+}
