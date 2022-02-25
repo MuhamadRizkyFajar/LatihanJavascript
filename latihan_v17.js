dbad9aae98d2d180499452ffc237d061a757db00
Muhamad Rizky Fajar <avangedsoul@gmail.com>
2022-02-26 00:16:37 +0700

    Signed-off-by: Muhamad Rizky Fajar <avangedsoul@gmail.com>

---
+++ latihan_v17.js
@@ -0,0 +1,11 @@
+alert('Hai!');
+alert('Nama');
+alert('Saya');
+alert('Chrome');
+var lagi = 1;
+
+while(lagi<=5){
+    var nama = prompt('siapa namamu?');
+    alert('Hai '+nama+'!');
+    lagi++
+}
---
+++ latihanjavas.js
@@ -0,0 +1,5 @@
+var awal = 1;
+while(awal<=10){
+    console.log('Angkot No. '+awal+' beroperasi dengan baik.');
+    awal++;
+}
