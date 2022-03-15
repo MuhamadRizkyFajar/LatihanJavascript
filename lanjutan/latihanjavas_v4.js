b226e26f0ef63231a3e79387e7a894f37e14681a
Irsyad Muhammad Andre <irsyad_vertix@apps.ipb.ac.id>
2022-03-15 16:14:16 +0700

    coret unpas javascript 15 maret 2022

--- lanjutan/latihanjavas_v4.js
+++ lanjutan/latihanjavas_v4.js
@@ -1,4 +1,4 @@
-//////HIGH ORDER FUNCTION
+//////HIGH ORDER FUNCTION------------------------------------------
 
 // function kerjakanTugas(matakuliah, selesai) {
 //   console.log(`Mulai mengerjakan tugas ${matakuliah}...`);
@@ -12,17 +12,44 @@
 // // let hasilkerjakantugas = kerjakanTugas(`fisika`, fxselesai);
 // kerjakanTugas(`fisika`, fxselesai);
 
-function ucapkansalam(waktu) {
-  return function (nama) {
-    console.log(`halo ${nama} dengan jam nya adalah ${waktu}`);
-  };
-}
-let selamatmalam = ucapkansalam(`malam`);
-console.dir(selamatmalam(`andre`));
-// console.dir(ucapkansalam().);
+// function ucapkansalam(waktu) {
+//   return function (nama) {
+//     console.log(`halo ${nama} dengan jam nya adalah ${waktu}`);
+//   };
+// }
+// let selamatmalam = ucapkansalam(`malam`);
+// console.dir(selamatmalam(`andre`));
+// // console.dir(ucapkansalam().);
 
-//////FILTER ,MAP, REDUCE
+//////FILTER ,MAP, REDUCE------------------------------------------
 
 // Array.prototype.map();
 // Array.prototype.filter();
 // Array.prototype.reduce();
+
+const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
+// const newangka = [];
+// for (let index = 0; index < angka.length; index++) {
+//   if (angka[index] >= 3) {
+//     newangka.push(angka[index]);
+//   }
+// }
+// console.log(newangka);
+
+// const newangka = angka.filter((a) => a > 3);
+// const newangka = angka.map((a) => a * 2);
+
+// const newangka = angka.reduce(
+//   (akumulator, kurenvalue) => akumulator + kurenvalue,
+// );
+
+// const newangkaa = angka.reduce()
+
+// console.log(newangka);
+
+const hasilcein = angka
+  .filter((a) => a > 5)
+  .map((a) => a * 3)
+  .reduce((acc, cur) => acc + cur);
+
+console.log(hasilcein);
